04ff4043c81fad3ba13e26366d69a41969355ce0
pravinjoshu <pravinvip003@gmail.com>
2025-12-16 18:30:50 +0530

    notification backend working

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -3,14 +3,12 @@ import { Order } from "../models/order.js";
 
 export const getNotifications = async (req, res) => {
   try {
-    console.log("GET /notifications API HIT");
+     
 
     const notifications = await Notification
       .find()
       .populate("orderId")
       .sort({ createdAt: -1 });
-
-    console.log("Notifications:", notifications.length);
 
     res.status(200).json({
       success: true,
