063ecdbe16b15e5c89ea3297714d8c750473e154
pravinjoshu <pravinvip003@gmail.com>
2026-01-08 18:03:34 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -28,9 +28,16 @@ connectDB();
 const app = express();
 
 // ✅ CORS
-app.use(cors({
-  origin: "*",   // later restrict pannalaam
-}));
+app.use(
+  cors({
+    origin: [
+      "https://cake-forest.netlify.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  })
+);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
